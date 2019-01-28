924633ab33ef506a3b02be1b3ab773b4940d29ec
Rubén Martínez <43479678+rubenmarsim@users.noreply.github.com>
2019-01-28 16:00:54 +0100

    .

--- Practica5/Practica5/Practica5/ViewModels/Base/DelegateCommand.cs
+++ Practica5/Practica5/Practica5/ViewModels/Base/DelegateCommand.cs
@@ -7,37 +7,65 @@ using Xamarin.Forms;
 
 namespace Practica5.ViewModels.Base
 {
+    /// <summary>
+    /// Clase para ejecutar comandos
+    /// </summary>
     public class DelegateCommand : ICommand
     {
+        /// <summary>
+        /// Declaramos un objeto action, que determina la accion que
+        /// se va a ejecutar
+        /// </summary>
         private readonly Action _execute;
+        /// <summary>
+        /// Determina, si se puede ejecutar o no el comando
+        /// </summary>
         private readonly Func<bool> _canExecute;
-
-        public DelegateCommand(Action execute) : this(execute, null) { }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="execute"></param>
+        public DelegateCommand(Action execute) : this(execute, null) { }        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="execute"></param>
+        /// <param name="canExecute"></param>
         public DelegateCommand(Action execute, Func<bool> canExecute)
         {
             _execute = execute;
             _canExecute = canExecute;
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
         public bool CanExecute(object parameter)
         {
             if (_canExecute != null) return _canExecute();
 
             return true;
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parameter"></param>
         public void Execute(object parameter)
         {
             _execute();
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
         public void RaiseCanExecuteChanged()
         {
             var handle = CanExecuteChanged;
             if (handle != null) handle(this, new EventArgs());
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
         public event EventHandler CanExecuteChanged;
     }
 }
